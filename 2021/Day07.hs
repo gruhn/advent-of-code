de0f76db23592961b8feed9e07175b5c1ea1d86c
Niklas Gruhn <niklas@gruhn.me>
2021-12-07 09:18:03 +0100

    [2021] solution day 7

---
+++ 2021/Day07.hs
@@ -0,0 +1,32 @@
+
+#!/usr/bin/env stack
+-- stack --resolver lts-18.18 script
+
+module Day07 where
+import ParseUtil (splitOn)
+
+parseInput :: String -> [Int]
+parseInput = map read . splitOn ","
+
+main :: IO ()
+main = do
+    input <- parseInput <$> readFile "07-input.txt"
+    putStr "Part 1: "
+    print $ minimize cost1 input
+    putStr "Part 2: "
+    print $ minimize cost2 input
+
+cost1 :: Int -> Int -> Int
+cost1 pos x = abs (x - pos)
+
+cost2 :: Int -> Int -> Int
+cost2 pos x = 
+    let n = cost1 pos x
+    in (n^2 + n) `div` 2
+
+minimize :: (Int -> Int -> Int) -> [Int] -> Int
+minimize cost xs =
+    let min = minimum xs
+        max = maximum xs
+        totalCost pos = sum $ map (cost pos) xs
+    in minimum $ map totalCost [min..max] 
