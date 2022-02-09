f30caea15ff6fe39a8b40425ab96aa9bd95dccbc
Niklas Gruhn <niklas@gruhn.me>
2022-02-09 13:07:22 +0100

    Fix solution day 6

--- 2021/Day06.hs
+++ 2021/Day06.hs
@@ -12,33 +12,23 @@ main = do
     input <- readFile "06-input.txt"
     let ages = parseInput input
     putStr "Part 1: "
-    print $ map (descendantCount 8) ages
+    print $ sum $ map (descendantCount 80) input
     putStr "Part 2: "
-    print $ map (descendantCount' 8) ages
-
-descendantCount :: Int -> Int -> Int
-descendantCount 0 age = 1
-descendantCount days 0 = descendantCount (days-1) 6 + descendantCount (days-1) 8
-descendantCount days age = descendantCount (days-1) (age-1)
-
-binom :: Int -> Int -> Int
+    print $ sum $ map (descendantCount 256) input
+  
+binom :: Integer -> Integer -> Integer
 binom n k 
     | k < 0     = 0
     | k > n     = 0
     | otherwise = product [(n-k+1)..n] `div` product [1..k]
-
-maxGenerations :: Int -> Int
-maxGenerations days = days `div` 7
-
-descendantCount' :: Int -> Int -> Int
-descendantCount' days age 
-    = sum 
-    . map (\(n,k) -> binom (n+k) k) 
-    $ map (\x -> (x, ((days+6-age) - 7*x) `div` 9)) [0..maxGenerations (days+6-age)]
     
-descendantCount'' :: Integer -> Integer -> Integer
-descendantCount'' days offset =
-  let days' = days - (offset + 1)
+descendantCount :: Integer -> Integer -> Integer
+descendantCount days age =
+  let -- remaining days adjusted for age:
+      days' = days - (age + 1)
+      -- maximum number of 8 day periods in ancestor chain:
       max8 = days' `div` (8+1)
+      -- maximum number of 6 day periods in ancestor chain given fixed number of 8s:
       max6 n = (days' - (8+1)*n) `div` (6+1)
   in 1 + sum [ binom (n+m) n | n <- [0 .. max8], m <- [0 .. max6 n] ]
+
