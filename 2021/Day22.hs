e51dd6ec8a34fe1d194d9a5d9862ca45ec52c067
Niklas Gruhn <niklas@gruhn.me>
2022-02-12 02:51:26 +0100

    [2021] solution day 22

---
+++ 2021/Day22.hs
@@ -0,0 +1,93 @@
+module Day22 where
+import Text.Parsec.String (Parser, parseFromFile)
+import Text.Parsec (string, many1, (<|>), newline, sepBy, char, option)
+import Text.Parsec.Char (digit)
+
+type Range = (Int, Int)
+
+data Cuboid = 
+    Cuboid Range Range Range
+    deriving Show
+
+intP :: Parser Int
+intP = do 
+    sign <- option "" (string "-")
+    digits <- many1 digit
+    return (read $ sign ++ digits)
+
+rangeP :: Parser Range
+rangeP = do
+    start <- intP
+    string ".."
+    end <- intP
+    return (start, end)
+
+cuboidStateP :: Parser Bool
+cuboidStateP = char 'o' >> (on <|> off)
+    where
+        on = string "n" >> return True
+        off = string "ff" >> return False
+
+cuboidP :: Parser (Bool, Cuboid)
+cuboidP = do
+    state <- cuboidStateP
+    string " x="
+    xRange <- rangeP
+    string ",y="
+    yRange <- rangeP
+    string ",z="
+    zRange <- rangeP
+    return (state, Cuboid xRange yRange zRange)
+
+insideInitArea :: Cuboid -> Bool
+insideInitArea (Cuboid (x0,xn) (y0,yn) (z0,zn)) =
+    all (-50 <=) [ x0, y0, z0 ] && 
+    all (<= 50) [ xn, yn, zn ]
+
+day22 :: IO ()
+day22 = do
+    instructions <- parseFromFile (cuboidP `sepBy` newline) "22-input.txt"
+    let resultVolume = sum . map volume . foldl applyInst []
+    putStr "Part 1: "
+    print $ resultVolume . filter (insideInitArea . snd) <$> instructions
+    putStr "Part 2: "
+    print $ resultVolume <$> instructions
+
+volume :: Cuboid -> Int
+volume (Cuboid (x0,xn) (y0,yn) (z0,zn)) =
+    let xl = xn-x0+1  
+        yl = yn-y0+1 
+        zl = zn-z0+1
+        sign = signum $ minimum [ xl, yl, zl ]
+    in sign * abs (xl*yl*zl)
+
+applyInst :: [Cuboid] -> (Bool, Cuboid) -> [Cuboid]
+applyInst cs (False, c) = concatMap (`difference` c) cs
+applyInst cs (True, c) = c : concatMap (`difference` c) cs 
+
+data Axis = X Int | Y Int | Z Int
+
+slice :: Axis -> Cuboid -> (Cuboid, Cuboid)
+slice (X axis) (Cuboid (x0, xn) yr zr) =
+    ( Cuboid (x0, min (axis-1) xn) yr zr
+    , Cuboid (max x0 axis,xn) yr zr
+    )
+slice (Y axis) (Cuboid xr (y0,yn) zr) =
+    ( Cuboid xr (y0, min (axis-1) yn) zr
+    , Cuboid xr (max y0 axis,yn) zr
+    )
+slice (Z axis) (Cuboid xr yr (z0,zn)) =
+    ( Cuboid xr yr (z0, min (axis-1) zn)
+    , Cuboid xr yr (max z0 axis, zn)
+    )
+
+difference :: Cuboid -> Cuboid -> [Cuboid]
+difference c0 (Cuboid (x0,xn) (y0,yn) (z0,zn)) = 
+    let (west, c1) = slice (X x0) c0
+        (c2, east) = slice (X (xn+1)) c1
+        (south, c3) = slice (Y y0) c2
+        (c4, north) = slice (Y (yn+1)) c3
+        (below, c5) = slice (Z z0) c4
+        (c6, above) = slice (Z (zn+1)) c5
+    in filter ((> 0) . volume)
+        [ west, east, north, south, above, below ] 
--- 2021/Main.hs
+++ 2021/Main.hs
@@ -1,11 +1,7 @@
 module Main where
 import Day15 (day15)
 import Day18 (day18)
-import Day21 (day21)
+import Day22 (day22)
 
 main :: IO ()
-main = day21
-
-step (n, p0, p1)
-    | even n    = (n+1, p0 + sum [ n*3+1, n*3+2, n*3+3 ], p1)
-    | otherwise = (n+1, p0 + sum [ n*3+1, n*3+2, n*3+3 ], p1)
+main = day22
