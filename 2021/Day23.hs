d57108ef1150d0968c0362cdadbfab6c561b7a69
Niklas Gruhn <niklas@gruhn.me>
2022-03-04 23:42:22 +0100

    2021 / day 23 / part 2

--- 2021/Day23.hs
+++ 2021/Day23.hs
@@ -1,245 +1,148 @@
-{-# LANGUAGE TupleSections #-}
 module Day23 (day23) where
-import Data.List (transpose, intersperse, partition)
-import Data.Tree (Tree (Node), unfoldTree)
-import Data.Maybe (mapMaybe)
-import Control.Monad.State
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Set (Set)
 import qualified Data.Set as Set
+import qualified Data.List as List
+import Algorithm.Search (aStarAssoc, dijkstraAssoc)
+import Data.Foldable
+import Data.Maybe (fromJust, mapMaybe)
 
 data Amphipod = A | B | C | D
-    deriving Show
+    deriving (Show, Eq, Ord)
 
-data Field = 
-    Free | Occupied Amphipod | Room [Amphipod] 
+type Pos = (Int, Int)
 
-type Board = [(Int, Field)]
+type Board = Map Pos Amphipod
 
-instance Show Field where
-    show Free = "."
-    show (Occupied amph) = show amph
-    show (Room amphs) = "."
+reachable :: Set Pos -> Board -> Pos -> Set Pos
+reachable area board start = Set.filter canHalt $ go start Set.empty where
+    notRoomEntry (x,0) = x `notElem` [2,4,6,8]
+    notRoomEntry _ = True
 
-showBoard :: Board -> String
-showBoard board =
-    let getRooms (Room xs) = xs
-        getRooms _ = []
+    canHalt pos = pos /= start && notRoomEntry pos
 
-        rooms   = filter (not . null) . map (getRooms . snd) $ board
-        
-        line1 = "#############"
-        line2 = ['#'] ++ concatMap (show . snd) board ++ ['#']
-        line34 = transpose 
-            $ ["# ", "# ", "##"] 
-            ++ intersperse "##" (map (concatMap show) rooms) 
-            ++ ["##", "# ", "# "]
-        line5 = "  #########  "
-    in unlines ([line1, line2] ++ line34 ++ [line5])
+    go :: Pos -> Set Pos -> Set Pos
+    go pos@(x,y) seenPos =
+        let isOutside = not $ pos `Set.member` area
+            alreadySeen = pos `Set.member` seenPos
+            isOccupied  = pos `Map.member` board && pos /= start
 
+            neighbors = Set.fromList
+                [ (x-1,y), (x+1,y), (x,y-1), (x,y+1) ]
+        in if isOutside || alreadySeen || isOccupied then
+            seenPos
+        else 
+            foldr go (Set.insert pos seenPos) neighbors
 
-initialBoard :: Board
-initialBoard = zip [0..]
-    [ Free, Free 
-    , Room [D,B]
-    , Free
-    , Room [C,A]
-    , Free
-    , Room [D,A]
-    , Free
-    , Room [B,C]
-    , Free, Free
-    ]
-
-neighborhood :: Int -> Board -> Board
-neighborhood index board =
-    let (left', right') = splitAt index board
-        left = reverse left'
-        right = tail right'
-
-        notOccupied (_, Occupied _) = False
-        notOccupied _ = True
-
-    in takeWhile notOccupied left 
-    ++ takeWhile notOccupied right
-
-reachable :: Int -> Int -> Board -> Bool
-reachable from to board =
-    elem to . map fst $ neighborhood from board
-
-setAt :: Int -> a -> [(Int, a)] -> [(Int, a)]
-setAt n x xs = 
-    let front = take n xs
-        rear  = drop (n+1) xs
-    in front ++ [(n,x)] ++ rear
-
-targetPos :: Amphipod -> Int
-targetPos amph = case amph of 
+targetRoomIndex :: Amphipod -> Int
+targetRoomIndex amph = case amph of
     A -> 2; B -> 4; C -> 6; D -> 8
+
+targetPos :: Amphipod -> Set Pos
+targetPos amph = Set.fromList
+    [ (targetRoomIndex amph, y) | y <- [-4 .. -1] ]
+
+hallway :: Set Pos
+hallway = Set.fromList
+    [ (x,0) | x <- [0..10] ] 
+
+rooms :: Set Pos
+rooms = Set.fromList
+    [ (x,y) | y <- [-4 .. -1], x <- [2,4,6,8] ]
+
+boardArea :: Set Pos
+boardArea = Set.union hallway rooms
+
+distance :: Pos -> Pos -> Int
+distance (x1, y1) (x2, y2) =
+    if x1 == x2 then
+        abs (y1 - y2)
+    else
+        abs (x1 - x2) + abs y1 + abs y2
 
 stepCost :: Amphipod -> Int
 stepCost amph = case amph of 
     A -> 1; B -> 10; C -> 100; D -> 1000
 
-buildSearchSpace :: Board -> (Board, [Board])
-buildSearchSpace board =
-    let pathCost :: Amphipod -> Int -> Int -> Int
-        pathCost amp from to = abs (from - to) * stepCost amp
+costLowerBound :: Board -> Int
+costLowerBound board = 
+    let pathCost :: Pos -> Amphipod -> Int
+        pathCost (x,y) amph
+            -- already in target room => count as negative cost
+            | x == targetRoomIndex amph = y * stepCost amph
+            | otherwise = distance (x,y) (targetRoomIndex amph, 0) * stepCost amph
+
+        moveToRoomCost = sum . Map.mapWithKey pathCost $ board 
+        enterRoomCost = sum . fmap (((4+3+2+1) *) . stepCost) $ [A,B,C,D]
+
+    in moveToRoomCost + enterRoomCost
+
+move :: Board -> Pos -> Pos -> Board
+move board from to = 
+    let fromVal = Map.lookup from board 
+        toVal = Map.lookup to board 
+    in case (fromVal, toVal) of
+        (Nothing, _) -> error "trying to move empty position"
+        (_, Just _) -> error "trying overwrite non-empty position"
+        (Just amph, Nothing) -> 
+            Map.delete from . Map.insert to amph $ board
+
+nextStates :: Board -> [(Board, Int)]
+nextStates board =
+    let isHallway (x,0) = True
+        isHallway _ = False
         
-        finish :: (Int, Field) -> [Board]
-        finish (index, Occupied amp) =
-            let target = targetPos amp
-                newBoard = setAt index Free board
-            in [ newBoard | reachable index target board ]
-        finish (index, Room (amp:amps)) =
-            let target = targetPos amp
-                newBoard = setAt index (Room amps) board
-            in [ newBoard | reachable index target board ]
-        finish _ = []
+        next :: (Pos, Amphipod) -> [(Board, Int)]
+        next (pos@(x,y), amph) =  
+            let otherRooms = rooms Set.\\ 
+                    if all (== amph) $ mapMaybe (`Map.lookup` board) $ Set.toList $ targetPos amph then
+                        targetPos amph
+                    else 
+                        Set.empty
+                span = reachable boardArea board pos Set.\\ otherRooms
+            in map (stateCost (pos, amph)) . Set.toList $ 
+                if isHallway pos then
+                    Set.intersection (targetPos amph) span
+                else
+                    span
 
-        spawn :: (Int, Field) -> [Board]
-        spawn (index, Room (amp:amps)) =
-            let isFree (_, Free) = True
-                isFree _ = False
+        stateCost :: (Pos, Amphipod) -> Pos -> (Board, Int)
+        stateCost (from, amph) to =
+            let newBoard = move board from to
+                cost = distance from to * stepCost amph
+            in (newBoard, cost)
 
-                occupy (target, _) = 
-                    setAt target (Occupied amp) board
-                
-            in map occupy 
-                $ filter isFree 
-                $ neighborhood index board
-        spawn _ = []
-        
-        next (_, Free) = []
-        next (_, Room []) = []
-        next pos = finish pos ++ spawn pos
+    in concatMap next $ Map.toAscList board
 
-    in (board, concatMap next board)
+initialState :: Board 
+initialState = Map.fromList 
+    [ ((2,-1),D), ((2,-2),D), ((2,-3),D), ((2,-4),B)
+    , ((4,-1),C), ((4,-2),C), ((4,-3),B), ((4,-4),A)
+    , ((6,-1),D), ((6,-2),B), ((6,-3),A), ((6,-4),A)
+    , ((8,-1),B), ((8,-2),A), ((8,-3),C), ((8,-4),C)
+    ]
 
--- buildSearchSpace :: (Int, Board) -> (Int, [(Int, Board)])
--- buildSearchSpace (cost, board) =
---     let pathCost :: Amphipod -> Int -> Int -> Int
---         pathCost amp from to = abs (from - to) * stepCost amp
-        
---         finish :: (Int, Field) -> [(Int, Board)]
---         finish (index, Occupied amp) =
---             let target = targetPos amp
---                 cost' = cost + pathCost amp index target
---                 newBoard = setAt index Free board
---             in [ (cost', newBoard) | reachable index target board ]
---         finish (index, Room (amp:amps)) =
---             let target = targetPos amp
---                 cost' = cost + pathCost amp index target
---                 newBoard = setAt index (Room amps) board
---             in [ (cost', newBoard) | reachable index target board ]
---         finish _ = []
-
---         spawn :: (Int, Field) -> [(Int, Board)]
---         spawn (index, Room (amp:amps)) =
---             let isFree (_, Free) = True
---                 isFree _ = False
-
---                 occupy (target, _) = 
---                     ( cost + pathCost amp index target
---                     , setAt target (Occupied amp) board )
-                
---             in map occupy 
---                 $ filter isFree 
---                 $ neighborhood index board
---         spawn _ = []
-        
---         next (_, Free) = []
---         next (_, Room []) = []
---         next pos = finish pos ++ spawn pos
-
---         allNext = concatMap next board 
---     in if null allNext then
---         (maxBound, [])
---     else 
---         (cost, allNext)
-
-searchSpace :: Tree Board
-searchSpace = unfoldTree buildSearchSpace initialBoard
-
-isFinalState :: Board -> Bool
-isFinalState board =
-    let nonEmptyRoom (_, Room (_:_)) = True
-        nonEmptyRoom _ = False
-    in not (any nonEmptyRoom board)
-
--- bestPath :: Tree Int -> Int -> State Int [Board]
--- bestPath (Node cost []) bestCost = state ([],)
--- bestPath (Node cost bs) bestCost = state $ \bestCost -> 
---     if cost >= bestCost then
---         ([], bestCost)
---     else 
---         foldM bestPath bestCost bs
-
-bestCost :: Tree Int -> Int -> Int
-bestCost (Node cost []) best = min cost best
-bestCost (Node cost bs) best
-    | cost < best = foldr bestCost best bs
-    | otherwise   = best
-
-fixedCost :: Int
-fixedCost =
-    let unpackRoom (Room as) = Just as
-        unpackRoom _ = Nothing
-
-        rooms = mapMaybe (unpackRoom . snd) initialBoard
-
-        startCost amps = sum 
-            $ zipWith (*) [1..] 
-            $ map stepCost amps
-
-        startCosts = sum
-            $ map startCost rooms
-
-        closeCosts = sum (map stepCost [A,B,C,D]) * 3
-    in startCosts + closeCosts
-
-data Pos = Hallway Int | RoomFirst Int | RoomLast Int
-
-type BoardState = Set.Set (Amphipod, Pos)
-
-targetRoom :: Amphipod -> Int
-targetRoom amph = case amph of 
-    A -> 2; B -> 4; C -> 6; D -> 8
-
-transitions :: BoardState -> [BoardState]
-transitions state = 
-    let isRoomFree :: Int -> Bool
-        isRoomFree roomIndex =
-            let matchRoom (_, RoomFirst idx) = idx == roomIndex
-                matchRoom (_, RoomLast idx) = idx == roomIndex
-                matchRoom _ = False
-            in null $ Set.filter matchRoom state
-
-        occupied :: [Int]
-        occupied = 
-            let hallwayPos (Hallway i) = [i]
-                hallwayPos _ = []
-            in concatMap hallwayPos $ Set.elems state
-
-        reachableFrom :: Int -> Int -> [Int]
-        reachableFrom from to =
-            
-
-
-        isOccupied :: Int -> Bool
-        isOccupied hallwayIndex = 
-            elem hallwayIndex occupied
-
-        
-        
-        next :: (Amphipod, Pos) -> [BoardState]
-        next s@(amph, Hallway index) =
-            [ Set.delete s state | isRoomFree (targetRoom amph) state && reachable index (targetRoom amph) ]
-    in concatMap next state 
-
-
+finalState :: Board 
+finalState = Map.fromList 
+    [ ((2,-1),A), ((2,-2),A), ((2,-3),A), ((2,-4),A)
+    , ((4,-1),B), ((4,-2),B), ((4,-3),B), ((4,-4),B)
+    , ((6,-1),C), ((6,-2),C), ((6,-3),C), ((6,-4),C)
+    , ((8,-1),D), ((8,-2),D), ((8,-3),D), ((8,-4),D)
+    ]
 
 day23 :: IO ()
-day23 = 
-    let unwrap (Node board _) = board
-    in do
-        putStr $ case searchSpace of 
-            (Node board bs) -> unlines $ map (showBoard . unwrap) bs
+day23 = do
+    let (cost, path) = fromJust $ aStarAssoc nextStates costLowerBound (== finalState) initialState
+    putStr $ unlines . map showBoard $ path
+    print cost
+
+showBoard :: Board -> String
+showBoard board =
+    let board' = Map.map show board
+        showPos pos = Map.findWithDefault "." pos (Map.map show board)
+
+        go (11,-4) = ""
+        go (11,y) = '\n' : go (0,y-1)
+        go (x,y)  = showPos (x,y) ++ go (x+1,y)
+    in '\n' : go (0,0)
