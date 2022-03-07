79a2003a2204689ff596c563e37e409390674303
Niklas Gruhn <niklas@gruhn.me>
2022-03-07 21:58:15 +0100

    2020 / day 6 / solutions

---
+++ 2020/Day06.hs
@@ -0,0 +1,32 @@
+module Day06 (day06) where
+
+import Text.Parsec (letter, many1, newline, sepBy, sepEndBy, endBy, many, choice, manyTill, count, oneOf)
+import Text.Parsec.String (Parser, parseFromFile)
+import qualified Data.Set as Set
+import Data.Set (Set)
+
+answersP :: Parser (Set Char)
+answersP = Set.fromList <$> 
+    many1 (oneOf ['a' .. 'z']) <* newline
+
+groupP :: Parser [Set Char]
+groupP = many1 answersP
+
+groupListP :: Parser [[Set Char]]
+groupListP = groupP `sepBy` newline
+
+unions :: [[Set Char]] -> [Set Char]
+unions = map Set.unions
+
+intersections :: [[Set Char]] -> [Set Char]
+intersections = map (foldr1 Set.intersection)
+
+day06 :: IO ()
+day06 = do
+    groups <- parseFromFile groupListP "input/06.txt"
+
+    putStr "Part 1: "
+    print $ sum . map Set.size . unions <$> groups
+
+    putStr "Part 2: "
+    print $ sum . map Set.size . intersections <$> groups
