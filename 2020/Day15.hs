0e9d156894750b6f952319df4599524f9eadef22
Niklas Gruhn <niklas@gruhn.me>
2022-06-03 23:56:55 +0200

    2020 / day 15 / slow solution

---
+++ 2020/Day15.hs
@@ -0,0 +1,19 @@
+module Day15 (day15) where
+
+import qualified Data.Map as Map
+import Data.Map (Map)
+
+turn :: Int -> Map Int Int -> Int -> [Int]
+turn i nums prev = 
+    let next = i - Map.findWithDefault i prev nums
+        nums' = Map.insert prev i nums
+    in next : (turn (i+1) nums' $! next)
+
+day15 :: IO ()
+day15 = do
+    let start = [0,20,7,16,1,18,15]
+        startMap = Map.fromList $ zip start [1..] 
+        result = start ++ turn (length start) startMap (last start)
+
+    print (result !! (2020-1))
+    print (result !! (30000000-1))
