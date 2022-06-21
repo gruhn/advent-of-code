ea67cfee7adb480b667a3cda59e3dcb883fe41c2
Niklas Gruhn <niklas@gruhn.me>
2022-06-21 23:51:13 +0200

    2020 / day 24 / solution

---
+++ 2020/Day24.hs
@@ -0,0 +1,56 @@
+module Day24 (parser, solver) where
+
+import Text.Megaparsec (Parsec, sepBy, some, choice)
+import Text.Megaparsec.Char (newline, string)
+import Data.Void (Void)
+import qualified Data.Set as Set
+import Data.Set (Set)
+import Vec
+
+type Tile = Vec2 Int
+
+parser :: Parsec Void String [Tile]
+parser = tiles where
+    dir = Vec2 <$> choice
+        [ (2,0)   <$ string "e"
+        , (1,-1)  <$ string "se"
+        , (-1,-1) <$ string "sw"
+        , (-2,0)  <$ string "w"
+        , (-1,1)  <$ string "nw"
+        , (1,1)   <$ string "ne" ]
+    tile = sum <$> some dir
+    tiles = tile `sepBy` newline
+
+flipTile :: Tile -> Set Tile -> Set Tile
+flipTile tile tiles
+    | Set.member tile tiles = Set.delete tile tiles
+    | otherwise             = Set.insert tile tiles
+
+neighbors :: Tile -> Set Tile
+neighbors v =
+    Set.fromList $ map ((+ v) . Vec2) [ (2,0), (1,-1), (-1,-1), (-2,0), (-1,1), (1,1) ]
+
+convolute :: Set Tile -> Set Tile
+convolute blackTiles =
+    let whiteTiles = Set.unions (Set.map neighbors blackTiles) Set.\\ blackTiles
+
+        blackNeighbors tile =
+            length $ Set.intersection (neighbors tile) blackTiles
+
+        blackRule n = not (n == 0 || n > 2)
+        whiteRule n = n == 2
+
+        keptBlackTiles = Set.filter (blackRule . blackNeighbors) blackTiles
+        addedBlackTiles = Set.filter (whiteRule . blackNeighbors) whiteTiles
+
+    in Set.union keptBlackTiles addedBlackTiles
+
+solver :: [Tile] -> IO ()
+solver coords = do
+    let blackTiles = foldr flipTile Set.empty coords
+
+    putStr "Part 1: "
+    print $ length blackTiles
+
+    putStr "Part 2: "
+    print $ length $ (!! 100) $ iterate convolute blackTiles
