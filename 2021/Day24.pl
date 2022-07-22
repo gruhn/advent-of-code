337bca50dfcd57c0841eee393f3c4484a4bf4a62
Niklas Gruhn <niklas@gruhn.me>
2022-07-22 05:46:54 +0200

    2021 / day 24 / refactor

--- 2021/Day24.pl
+++ 2021/Day24.pl
@@ -3,40 +3,43 @@
 :- use_module(library(pio), [phrase_from_file/2]).
 :- use_module(library(charsio), [char_type/2]).
 
-digits_([D|T]) --> digit_(D), !, digits_(T). 
-digits_([])    --> [].
+digits([D|Ds]) --> digit(D), digits(Ds). 
+digits([])     --> [].
 
-digit_(D) --> [D], { char_type(D, decimal_digit) }.
+digit(D) --> [D], { char_type(D, decimal_digit) }.
 
-uint_(Int) --> digit_(D), digits_(Ds), { number_chars(Int, [D|Ds]) }.
+integer(Int) --> digit(D), digits(Ds), { number_chars(Int, [D|Ds]) }.
 
-int_(NegNum) --> "-", !, uint_(Num), { NegNum #= Num * (-1) }.
-int_(PosNum) --> uint_(PosNum).
+signed_integer(NegNum) --> "-", integer(Num), { NegNum #= Num * (-1) }.
+signed_integer(PosNum) --> integer(PosNum).
 
-register_(Char) --> [Char].
+register(reg(Char)) --> [Char].
 
-value_(#Val) --> int_(Val), !.
-value_(Var)  --> register_(Var).
+value(#Val)     --> signed_integer(Val).
+value(reg(Var)) --> [Var].
 
-op_((inp, Reg))        --> "inp ", !, register_(Reg).
-op_((add, Reg, Val))   --> "add ", !, register_(Reg), " ", value_(Val).
-op_((mul, Reg, Val))   --> "mul ", !, register_(Reg), " ", value_(Val).
-op_(((mod), Reg, Val)) --> "mod ", !, register_(Reg), " ", value_(Val).
-op_(((div), Reg, Val)) --> "div ", !, register_(Reg), " ", value_(Val).
-op_((eql, Reg, Val))   --> "eql ", !, register_(Reg), " ", value_(Val).
+binary_instruction(add)   --> "add".
+binary_instruction(mul)   --> "mul".
+binary_instruction((mod)) --> "mod".
+binary_instruction((div)) --> "div".
+binary_instruction(eql)   --> "eql".
 
-lines_([L|Ls]) --> op_(L), "\n", !, lines_(Ls).
-lines_([L])    --> op_(L), !.
-lines_([])     --> [].
+instruction((inp, reg(Reg)))       --> "inp ", [Reg].
+instruction((Inst, reg(Reg), Val)) -->
+    binary_instruction(Inst), " ", [Reg], " ", value(Val).
 
-store(w, #W, (_,X,Y,Z), (W,X,Y,Z)).
-store(x, #X, (W,_,Y,Z), (W,X,Y,Z)).
-store(y, #Y, (W,X,_,Z), (W,X,Y,Z)).
-store(z, #Z, (W,X,Y,_), (W,X,Y,Z)).
+instructions([L|Ls]) --> instruction(L), "\n", instructions(Ls).
+instructions([L])    --> instruction(L).
+instructions([])     --> [].
+
+store(reg(w), #W, (_,X,Y,Z), (W,X,Y,Z)).
+store(reg(x), #X, (W,_,Y,Z), (W,X,Y,Z)).
+store(reg(y), #Y, (W,X,_,Z), (W,X,Y,Z)).
+store(reg(z), #Z, (W,X,Y,_), (W,X,Y,Z)).
 
 load(#Val, _, #Val).
-load(Var, State, #Val) :-
-    store(Var, #Val, _, State).
+load(reg(Var), State, #Val) :-
+    store(reg(Var), #Val, _, State).
 
 op_calc((add),A,B,C) :- C #= A + B.
 op_calc((mul),A,B,C) :- C #= A * B. 
@@ -44,22 +47,26 @@ op_calc((mod),A,B,C) :- C #= A mod B.
 op_calc((div),A,B,C) :- C #= A div B.
 op_calc((eql),A,B,C) :- C #<==> (A #= B).
 
-run_([], _, StateN, StateN).
-run_([(inp, Reg)|Ops], [Var|Input], State0, StateN) :-
-    store(Reg, #Var, State0, State1),
-    run_(Ops, Input, State1, StateN).
-run_([(Op, RegA, RegB)|Ops], Input, State0, StateN) :-
-    load(RegA, State0, A),
-    load(RegB, State0, B),
-    op_calc(Op, A, B, Out),
-    store(RegA, #Out, State0, State1),
-    run_(Ops, Input, State1, StateN).
+state(S0, S), [S] --> [S0].
+
+run_([], _) --> [].
+run_([(inp, Reg)|Ops], [Var|Input]) -->
+    state(State0, State1),
+    { store(Reg, #Var, State0, State1) },
+    run_(Ops, Input).
+run_([(Op, RegA, RegB)|Ops], Input) -->
+    state(State0, State1),
+    { load(RegA, State0, A)
+    , load(RegB, State0, B)
+    , op_calc(Op, A, B, Out)
+    , store(RegA, #Out, State0, State1) },
+    run_(Ops, Input).
 
 run(Input) :-
-    phrase_from_file(lines_(Ops), "24-input.txt"),
+    once(phrase_from_file(instructions(Program), "24-input.txt")),
     length(Input, 14),
     Input ins 1..9,
-    run_(Ops, Input, (0,0,0,0), (_,_,_,0)).
+    phrase(run_(Program, Input), [(0,0,0,0)], [(_,_,_,0)]).
 
 lift(F, V, Lifted) :-
     Lifted =.. [F, V].
