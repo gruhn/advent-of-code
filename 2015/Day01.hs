eeea0e42f45f55fd14108e04897caef8744ac9db
Niklas Gruhn <niklas@gruhn.me>
2022-07-22 22:54:00 +0200

    2015 / day 01 / solution

---
+++ 2015/Day01.hs
@@ -0,0 +1,20 @@
+module Main where
+import Text.Megaparsec (Parsec, (<|>), many, parse)
+import Data.Void (Void)
+import Text.Megaparsec.Char (char)
+
+parser :: Parsec Void String [Int]
+parser = 
+    let up = 1 <$ char '('
+        down = -1 <$ char ')'
+    in  many (up <|> down)
+
+main :: IO ()
+main = do
+    input <- parse parser "" <$> readFile "2015/input/01.txt"
+
+    putStr "Part 1: "
+    print $ sum <$> input
+
+    putStr "Part 2: "
+    print $ length . takeWhile (>= 0) . scanl (+) 0 <$> input
