e1d4195d28dd32fbb46433be395625a342069ec0
Niklas Gruhn <niklas@gruhn.me>
2022-07-28 21:51:15 +0200

    2015 / day 19 / tinker

---
+++ 2015/Day19.hs
@@ -0,0 +1,114 @@
+module Main where
+import Text.Megaparsec (Parsec, some, sepBy, parse, errorBundlePretty, sepEndBy)
+import Data.Void (Void)
+import Text.Megaparsec.Char (hspace, letterChar, string, newline)
+import Data.List (stripPrefix, mapAccumR)
+import qualified Data.Set as Set
+import Data.Tree (Tree (Node, rootLabel), unfoldTree, drawTree)
+import Data.Function ((&))
+import Data.Maybe (mapMaybe)
+import Control.Applicative (Applicative(liftA2))
+import Data.Tuple (swap)
+
+type Parser = Parsec Void String
+
+type Rule = (String,String)
+
+parser :: Parser ([Rule], String)
+parser =
+    let rule :: Parser Rule
+        rule = (,)
+            <$> some letterChar 
+            <*  string " => " 
+            <*> some letterChar 
+        
+        rules :: Parser [Rule]
+        rules = rule `sepEndBy` newline
+
+        molecule :: Parser String
+        molecule = newline *> some letterChar
+
+    in  (,) <$> rules <*> molecule
+
+replacementsWith :: Rule -> String -> [String]
+replacementsWith (match,replace) [] = []
+replacementsWith (match,replace) (c:cs) = 
+    case stripPrefix match (c:cs) of
+        Nothing   -> (c:) <$> replacementsWith (match,replace) cs
+        Just rest -> 
+            let rep  = replace ++ rest
+                reps = (c:) <$> replacementsWith (match,replace) cs 
+            in  rep : reps
+
+replacements :: [Rule] -> String -> [String]
+replacements rules molecule =
+    concatMap (`replacementsWith` molecule) rules
+
+distinct :: [String] -> [String]
+distinct = Set.toList . Set.fromList
+
+replacementTree :: [Rule] -> String -> Tree String
+replacementTree rules = 
+    unfoldTree (\str -> (str, replacements rules str))
+
+pruneDepth :: Int -> Tree String -> Tree String
+pruneDepth 0 (Node str _) = Node str []
+pruneDepth d (Node str children) = 
+    Node str (pruneDepth (d-1) <$> children)
+
+-- pruneLength :: Int -> Tree String -> Tree String
+-- pruneLength l (Node str children) =
+--     let children' = filter ((l >=) . length . rootLabel) children
+--     in  Node str (pruneLength l <$> children')
+
+pruneDuplicates :: Tree String -> Tree String
+pruneDuplicates =
+    let go :: Set.Set String -> Tree String -> (Set.Set String, Tree String)
+        go seen0 (Node str children0) =
+            let seen1 = Set.insert str seen0
+                notSeen = (`Set.notMember` seen1) . rootLabel
+
+                children1 = filter notSeen children0
+                (seen2, children2) = mapAccumR go seen1 children1
+
+            in  (seen2, Node str children2)
+    in  snd . go Set.empty
+
+shortestPath :: [Rule] -> String -> String -> Maybe Int
+shortestPath rules start end =
+    let searchSpace = replacementTree rules start
+            -- & pruneLength (length end)
+            & pruneDuplicates
+
+        go :: Int -> Tree String -> Maybe Int
+        go depth (Node str children)
+            | str == end = Just depth
+            | otherwise  = 
+                let minJust :: Tree String -> Maybe Int -> Maybe Int
+                    minJust child Nothing = go (depth+1) child
+                    minJust child (Just bestDepth) =
+                        case go (depth+1) (pruneDepth (bestDepth-1) child) of
+                            Nothing -> Just bestDepth
+                            Just bestDepth' -> Just (min bestDepth bestDepth')
+                in  foldr minJust Nothing children
+
+    in  go 0 searchSpace
+
+main :: IO ()
+main = do
+    input <- parse parser "" <$> readFile "2015/input/19.txt"
+    case input of
+        Left err -> putStr (errorBundlePretty err)
+        Right (rules,molecule) -> do
+            -- putStr "Part 1: "
+            -- print $ length $ distinct $ replacements rules molecule
+
+            putStr "Part 2: "
+            let rules' = swap <$> rules
+            print $ shortestPath rules' molecule "e"
+
+            -- print $ replacements rules molecule
+            -- putStr $ drawTree
+            --        $ pruneDuplicates
+            --        -- $ pruneLength (length molecule)
+            --        $ replacementTree rules' molecule
