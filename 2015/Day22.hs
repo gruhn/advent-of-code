6fd1c4415a77df4c0f7e09479b56ff2fb4065860
Niklas Gruhn <niklas@gruhn.me>
2022-08-03 22:19:27 +0200

    2015 / day 22 / tinker

--- 2015/Day22.hs
+++ 2015/Day22.hs
@@ -2,32 +2,33 @@ module Main where
 import Lens.Micro (lens, over, set, (^.), Lens')
 import Data.Function ((&))
 import Data.Functor ((<&>))
-import Data.Foldable (find)
+import Data.Foldable (find, for_)
 import Data.Tree (unfoldForest, Forest, Tree (Node, subForest), unfoldTree, drawTree, drawForest)
 import Data.Maybe (fromMaybe)
-import Data.List (mapAccumR)
+import Data.List (mapAccumR, (\\))
+import Data.Map (Map)
+import qualified Data.Map as Map
 
 data Spell = MagicMissle | Drain | Shield | Poison | Recharge
-    deriving (Eq, Show)
+    deriving (Eq, Ord, Show)
 
-data Effect = Effect
-    { _timer :: Int
-    , _spell :: Spell
-    } deriving Show
+spells :: [Spell]
+spells = [ Shield, Drain, Poison, MagicMissle, Recharge ]
+
+manaCost :: Spell -> Int
+manaCost MagicMissle = 53
+manaCost Drain = 73
+manaCost Shield = 113
+manaCost Poison = 173
+manaCost Recharge = 229
 
 data GameState = GS
     { _mana :: Int
     , _hitPoints :: Int
     , _bossDamage :: Int
     , _bossHitPoints :: Int
-    , _effects :: [Effect]
+    , _effects :: Map Spell Int
     } deriving Show
-
-timer :: Lens' Effect Int
-timer = lens _timer (\state v -> state { _timer = v })
-
-spell :: Lens' Effect Spell
-spell = lens _spell (\state v -> state { _spell = v })
 
 mana :: Lens' GameState Int
 mana = lens _mana (\state v -> state { _mana = v })
@@ -41,8 +42,11 @@ bossDamage = lens _bossDamage (\state v -> state { _bossDamage = v })
 bossHitPoints :: Lens' GameState Int
 bossHitPoints = lens _bossHitPoints (\state v -> state { _bossHitPoints = v })
 
-effects :: Lens' GameState [Effect]
+effects :: Lens' GameState (Map Spell Int)
 effects = lens _effects (\state v -> state { _effects = v })
+
+insertEffect :: Spell -> Int -> Map Spell Int -> Map Spell Int
+insertEffect = Map.insertWith (\tNew tOld -> if tOld == 0 then tNew else tOld)
 
 castSpell :: Spell -> GameState -> GameState
 -- Magic Missile costs 53 mana. It instantly does 4 damage.
@@ -58,43 +62,42 @@ castSpell Drain state = state
 -- While it is active, your armor is increased by 7.
 castSpell Shield state = state
     & over mana (+ (-113))
-    & over effects (Effect 6 Shield :)
+    & over effects (insertEffect Shield 6)
 -- Poison costs 173 mana. It starts an effect that lasts for 6 turns. 
 -- At the start of each turn while it is active, it deals the boss 3 damage.
 castSpell Poison state = state
     & over mana (+ (-173))
-    & over effects (Effect 6 Poison :)
+    & over effects (insertEffect Poison 6)
 -- Recharge costs 229 mana. It starts an effect that lasts for 5 turns. 
 -- At the start of each turn while it is active, it gives you 101 new mana.
 castSpell Recharge state = state
     & over mana (+ (-229))
-    & over effects (Effect 5 Recharge :)
+    & over effects (insertEffect Recharge 5)
 
-applyEffect :: Effect -> GameState -> GameState
-applyEffect (Effect _ Poison) state = state
+applyEffect :: Spell -> GameState -> GameState
+applyEffect Poison state = state
     & over bossHitPoints (+ (-3))
-applyEffect (Effect _ Recharge) state = state
+applyEffect Recharge state = state
     & over mana (+ 101)
 applyEffect _ state = state
 
 applyEffects :: GameState -> GameState
 applyEffects state =
-    foldr applyEffect state (state ^. effects)
+    foldr applyEffect state (state ^. effects & Map.keys)
 
 decrementTimers :: GameState -> GameState
 decrementTimers state = state
-    & over (effects . traverse . timer) (+ (-1))
-    & over effects (filter ((>0) . (^. timer)))
+    & over (effects . traverse) (+ (-1))
+    & over effects (Map.filter (>=0))
 
 armor :: GameState -> Int
-armor state =
-    let getArmor (Effect _ Shield) = 7
-        getArmor _ = 0
-    in  state ^. effects <&> getArmor & sum
+armor state
+    | Map.member Shield (state ^. effects) = 7
+    | otherwise = 0
 
 damage :: GameState -> Int
 damage state =
-     state ^. bossDamage - armor state
+    max 1 (state ^. bossDamage - armor state)
 
 bossAttack :: GameState -> GameState
 bossAttack state =
@@ -102,8 +105,9 @@ bossAttack state =
 
 movesPerRound :: Spell -> [GameState -> GameState]
 movesPerRound spell =
-    [ applyEffects, decrementTimers, castSpell spell -- player turn
-    , applyEffects, decrementTimers, bossAttack ]    -- boss turn
+    [ over hitPoints (+ (-1))
+    , decrementTimers, applyEffects, castSpell spell -- player turn
+    , decrementTimers, applyEffects, bossAttack ]    -- boss turn
 
 movesPerGame :: [Spell] -> [GameState -> GameState]
 movesPerGame = concatMap movesPerRound
@@ -112,13 +116,15 @@ isBossWin :: GameState -> Bool
 isBossWin state =
     let bossDead   s = s ^. bossHitPoints <= 0
         playerDead s = s ^. hitPoints <= 0
-        manaEmpty  s = s ^. mana < 0
-    in not (bossDead state) && (playerDead state || manaEmpty state)
+        -- manaEmpty  s = s ^. mana < 0
+    in  not (bossDead state) && playerDead state
 
 isPlayerWin :: GameState -> Bool
 isPlayerWin state =
     let bossDead   s = s ^. bossHitPoints <= 0
         playerDead s = s ^. hitPoints <= 0
+        -- manaEmpty  s = s ^. mana < 0
+    -- in  bossDead state && not (playerDead state || manaEmpty state)
     in  bossDead state && not (playerDead state)
 
 isWinState :: GameState -> Bool
@@ -136,30 +142,48 @@ play strategy state0 = strategy
     & scanl (flip ($)) state0
     & endState
 
+play' :: [Spell] -> GameState -> [GameState]
+play' strategy state0 = strategy
+    & movesPerGame
+    & scanl (flip ($)) state0
+
 strategySpace :: GameState -> Forest Spell
 strategySpace state0 =
-    let spells = [ Shield, Drain, Poison, MagicMissle, Recharge ]
+    let build :: (GameState, Spell) -> ((GameState, Spell), [(GameState, Spell)])
+        build (state0, nextSpell) =
+            let stateN = play [nextSpell] state0
 
-        build :: (GameState, Spell) -> ((GameState, Spell), [(GameState, Spell)])
-        build (state0, spell) =
-            let stateN = play [spell] state0
-            in  ((stateN, spell), zip (repeat stateN) spells)
+                activeEffects = stateN ^. effects
+                    & Map.filter (> 0)
+                    & Map.keys
 
-        prune :: Forest (GameState, Spell) -> Forest (GameState, Spell)
-        prune [] = []
-        prune (Node (state, spell) children : trees)
-            | isBossWin state = prune trees
-            | isPlayerWin state = Node (state, spell) [] : prune trees
-            | otherwise = case prune children of
-                []        -> prune trees
-                children' -> Node (state, spell) children' : prune trees
+                possibleSpells = spells
+                    & filter (`notElem` activeEffects)
+                    & filter ((stateN ^. mana >) . manaCost)
+
+                children
+                    | isWinState stateN = []
+                    | otherwise = zip (repeat stateN) possibleSpells
+
+            in  ((stateN, nextSpell), children)
 
         space = zip (repeat state0) spells
             & fmap (unfoldTree build)
-            & prune
+            & pruneBottomUp' (isPlayerWin . fst)
+            -- & pruneBottomUp (isBossWin . fst)
             & fmap (fmap snd)
 
     in  space
+
+pruneBottomUp' :: (a -> Bool) -> Forest a -> Forest a
+pruneBottomUp' keep [] = []
+pruneBottomUp' keep (Node label [] : trees)
+    | keep label = Node label [] : pruneBottomUp' keep trees
+    | otherwise  = pruneBottomUp' keep trees
+pruneBottomUp' keep (Node label children : trees) =
+    case pruneBottomUp' keep children of
+        [] -> pruneBottomUp' keep trees
+        cs -> Node label cs : pruneBottomUp' keep trees
 
 pruneBottomUp :: (a -> Bool) -> Forest a -> Forest a
 pruneBottomUp reject [] = []
@@ -174,18 +198,11 @@ pruneBottomUp reject (Node label children : trees)
 
 search :: Forest Spell -> (Int, Forest Spell)
 search forest =
-    let manaCost :: Spell -> Int
-        manaCost MagicMissle = 53
-        manaCost Drain = 73
-        manaCost Shield = 113
-        manaCost Poison = 173
-        manaCost Recharge = 229
-
-        withPathCost :: (a -> Int) -> Int -> Forest a -> Forest (Int, a)
+    let withPathCost :: (a -> Int) -> Int -> Forest a -> Forest (Int, a)
         withPathCost f ancestorCost [] = []
-        withPathCost f ancestorCost (Node label children : trees) = 
+        withPathCost f ancestorCost (Node label children : trees) =
             let children' = withPathCost f (ancestorCost + f label) children
-                node' = Node (ancestorCost + f label, label) children' 
+                node' = Node (ancestorCost + f label, label) children'
             in  node' : withPathCost f ancestorCost trees
 
         minPath :: Int -> Forest (Int, a) -> Int
@@ -198,7 +215,7 @@ search forest =
             | otherwise    = minPath (minPath best children) trees
 
         withPathCost' = withPathCost manaCost 0 forest
-        minPathCost = minPath 10000 withPathCost' 
+        minPathCost = minPath 10000 withPathCost'
         forest' = (snd <$>) <$> pruneBottomUp ((> minPathCost) . fst) withPathCost'
 
     in  (minPathCost, forest')
@@ -206,33 +223,34 @@ search forest =
 main :: IO ()
 main = do
 
-    let initialState = GS
+    let state0 = GS
             { _mana = 500
             , _hitPoints = 50
             , _bossDamage = 9
             , _bossHitPoints = 51
-            , _effects = []
+            , _effects = Map.empty
             }
 
-   -- let initialState = GS
+    -- let state0 = GS
     --         { _mana = 250
     --         , _hitPoints = 10
     --         , _bossDamage = 8
     --         , _bossHitPoints = 14
-    --         , _effects = []
+    --         , _effects = Map.empty
     --         }
 
-    -- print $ play [ Recharge, Shield, Drain, Poison, MagicMissle ] initialState
-    -- print $ play [Drain, Shield] initialState
+    putStrLn "Part 2: "
 
-    -- putStr $ drawForest 
-    --     $ fmap (fmap show) 
-    --     $ snd . search
-    --     $ strategySpace initialState
+    putStr $ drawForest
+        $ fmap (fmap show)
+        $ snd . search
+        $ strategySpace state0
 
-    print 
+    print
         $ fst . search
-        $ strategySpace initialState
+        $ strategySpace state0
 
+    -- for_ (play' [Poison, MagicMissle, MagicMissle, Poison, MagicMissle] state0) print
 
--- 854 too low
+-- 1013 too low
+-- 1242 too high
