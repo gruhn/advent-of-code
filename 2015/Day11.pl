4327a9bc28ae0b2cb6c39670878387e87ac1e101
Niklas Gruhn <niklas@gruhn.me>
2022-08-10 00:04:51 +0200

    2015 / day 11 / refactor

--- 2015/Day11.pl
+++ 2015/Day11.pl
@@ -1,6 +1,7 @@
 :- use_module(library(clpz)).
 :- use_module(library(lists)).
 :- use_module(library(dcgs)).
+:- use_module(library(time)).
 
 not_ins(_, []).
 not_ins(Xs, [Y|Ys]) :-
@@ -20,14 +21,14 @@ straight([C1,C2|Cs]) :-
     C1 #= C2 + 1,
     straight([C2|Cs]).
 
-after([C|Cs], [C|Ds]) :- 
-    same_length(Cs, Ds), 
-    after(Cs, Ds).
 after([C|Cs], [D|Ds]) :- 
     same_length(Cs, Ds), 
     C #< D.
+after([C|Cs], [C|Ds]) :- 
+    same_length(Cs, Ds), 
+    after(Cs, Ds).
 
-password(OldCodes, Codes) :-
+password(Codes) :-
     % passwords must be exactly eight lowercase letters
     length(Codes, 8),
     char_code_domain(a, z, A_to_Z),
@@ -36,52 +37,30 @@ password(OldCodes, Codes) :-
     % passwords may not contain the letters i, o, or l
     chars_codes("iol", Excluded),
     not_ins(Codes, Excluded),
-
-    % finds new password by incrementing old password
-    after(OldCodes, Codes),
-    reverse(Codes, Codes1),
+    reverse(Codes, CodesRev),
 
     % passwords must include one increasing straight of at least three letters
     length(Straight, 3),
     straight(Straight),
-    phrase((...,seq(Straight),...), Codes1),
+    phrase((...,seq(Straight),...), CodesRev),
 
     % passwords must contain at least two different, non-overlapping pairs
-    phrase((...,seq([X,X]),...,seq([Y,Y]),...), Codes).
+    phrase((...,seq([X,X]),...,seq([Y,Y]),...), CodesRev).
 
-wrap_min(X, min(X)).
+% FIXME: enumeration is a bit off. In the beginning solutions are repeated. Why?
+password_between(Codes, LowerBound, UpperBound) :-
+    password(Between),
+    after(LowerBound, Between),
+    after(Between, UpperBound),
+
+    label(Between),
+
+    ( password_between(Codes, LowerBound, Between) 
+    ; Codes = Between
+    ; password_between(Codes, Between, UpperBound)
+    ).
 
 solution(NextPassword) :-
     chars_codes("vzbxkghb", OldCodes),
-    password(OldCodes, NextCodes),
-
-    % FIXME: expected output is
-    %
-    %  ?- solution(Next).
-    %  ;  Next = "vzbxxyzz"
-    %  ;  Next = "vzcaabcc"
-    %  ;  ...
-    %
-    % but we get 
-    %
-    %  ?- solution(Next).
-    %  ;  Next = "vzbxxyzz"
-    %  ;  Next = "vzzaaabc"
-    %  ;  Next = "vzzaabcd" 
-    %  ;  ...
-    %
-    % i.e. the enumeration jumps over some smaller
-    % values. By manually constraining the result
-    % with upper bounds we can find find the smallest 
-    % successor and prove that it's the smallest but
-    % would be nicer to make fix this.
-
-    chars_codes("vzzaaabc", UpperBound1),
-    after(NextCodes, UpperBound1),
-
-    chars_codes("vzccaabc", UpperBound2),
-    after(NextCodes, UpperBound2),
-
-    maplist(wrap_min, NextCodes, Mins),
-    labeling(Mins, NextCodes),
-    chars_codes(NextPassword, NextCodes).
+    password_between(Codes, OldCodes, BoundCodes),
+    chars_codes(NextPassword, Codes).
