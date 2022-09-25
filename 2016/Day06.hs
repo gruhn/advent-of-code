24f59b763c6f9e31f64b17ba18b673093eebccd1
Niklas Gruhn <niklas@gruhn.me>
2022-09-25 14:42:02 +0200

    2016 / day 06 / solution

---
+++ 2016/Day06.hs
@@ -0,0 +1,19 @@
+module Main where
+import Data.List (group, sort, maximumBy, transpose, minimumBy)
+import Control.Arrow ((&&&))
+import Data.Function (on)
+
+itemsWithFrequency :: Ord a => [a] -> [(Int, a)]
+itemsWithFrequency = 
+  sort . fmap (length &&& head) . group . sort
+
+main :: IO ()
+main = do
+  rows <- lines <$> readFile "2016/input/06.txt"
+  let columns = transpose rows
+
+  putStr "Part 1: "
+  print $ snd . head . itemsWithFrequency <$> columns
+
+  putStr "Part 2: "
+  print $ snd . last . itemsWithFrequency <$> columns
