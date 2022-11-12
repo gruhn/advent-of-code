574a1a13c6f8dfabf7d04afca9f233e4e681e414
Niklas Gruhn <niklas@gruhn.me>
2022-11-12 10:37:58 +0100

    2021 / day 24 / refactor

--- 2021/Day24.pl
+++ 2021/Day24.pl
@@ -3,84 +3,106 @@
 :- use_module(library(pio), [phrase_from_file/2]).
 :- use_module(library(charsio), [char_type/2]).
 
-digits([D|Ds]) --> digit(D), digits(Ds). 
-digits([])     --> [].
+program(Program) :-
+    phrase_from_file(instructions(Program), "24-input.txt").
+
+part1(Input) :- 
+    program(Program),
+
+    length(Input, 14),
+    Input ins 1..9,
+
+    run(Program, Input, [_,_,_,0]), 
+
+    maximum(Input).
+
+part2(Input) :-
+    program(Program),
+
+    length(Input, 14),
+    Input ins 1..9,
+
+    run(Program, Input, [_,_,_,0]), 
+
+    minimum(Input).
+
+% --------------------------------------------
+% Parser
+% --------------------------------------------
 
 digit(D) --> [D], { char_type(D, decimal_digit) }.
 
-integer(Int) --> digit(D), digits(Ds), { number_chars(Int, [D|Ds]) }.
+digits([D|Ds]) --> digit(D), digits(Ds). 
+digits([])     --> [].
 
-signed_integer(NegNum) --> "-", integer(Num), { NegNum #= Num * (-1) }.
-signed_integer(PosNum) --> integer(PosNum).
+decimal(Dec) --> digit(D), digits(Ds), { number_chars(Dec, [D|Ds]) }.
+
+integer(NegNum) --> "-", decimal(Num), { NegNum #= Num * (-1) }.
+integer(PosNum) --> decimal(PosNum).
 
 register(reg(Char)) --> [Char].
 
-value(#Val)     --> signed_integer(Val).
-value(reg(Var)) --> [Var].
+value_or_register(#Val)     --> integer(Val).
+value_or_register(reg(Reg)) --> [Reg].
 
-binary_instruction(add)   --> "add".
-binary_instruction(mul)   --> "mul".
-binary_instruction((mod)) --> "mod".
-binary_instruction((div)) --> "div".
-binary_instruction(eql)   --> "eql".
+operator(add) --> "add".
+operator(mul) --> "mul".
+operator(mod) --> "mod".
+operator(div) --> "div".
+operator(eql) --> "eql".
 
-instruction((inp, reg(Reg)))       --> "inp ", [Reg].
-instruction((Inst, reg(Reg), Val)) -->
-    binary_instruction(Inst), " ", [Reg], " ", value(Val).
+instruction(inp-Target)       --> "inp ", register(Target).
+instruction(Op-Target-Source) -->
+    operator(Op), " ", register(Target), " ", value_or_register(Source).
 
-instructions([L|Ls]) --> instruction(L), "\n", instructions(Ls).
-instructions([L])    --> instruction(L).
-instructions([])     --> [].
+instructions([In|Ins]) --> instruction(In), "\n", instructions(Ins).
+instructions([In])     --> instruction(In).
+instructions([])       --> [].
 
-store(reg(w), #W, (_,X,Y,Z), (W,X,Y,Z)).
-store(reg(x), #X, (W,_,Y,Z), (W,X,Y,Z)).
-store(reg(y), #Y, (W,X,_,Z), (W,X,Y,Z)).
-store(reg(z), #Z, (W,X,Y,_), (W,X,Y,Z)).
+% --------------------------------------------
+% Interpreter
+% --------------------------------------------
 
-load(#Val, _, #Val).
-load(reg(Var), State, #Val) :-
-    store(reg(Var), #Val, _, State).
+store(reg(w), #W), [W,X,Y,Z] --> [_,X,Y,Z].
+store(reg(x), #X), [W,X,Y,Z] --> [W,_,Y,Z].
+store(reg(y), #Y), [W,X,Y,Z] --> [W,X,_,Z].
+store(reg(z), #Z), [W,X,Y,Z] --> [W,X,Y,_].
 
-op_calc((add),A,B,C) :- C #= A + B.
-op_calc((mul),A,B,C) :- C #= A * B. 
-op_calc((mod),A,B,C) :- C #= A mod B. 
-op_calc((div),A,B,C) :- C #= A div B.
-op_calc((eql),A,B,C) :- C #<==> (A #= B).
+load(#Val, #Val, State0, State0).
+load(reg(Var), #Val, State0, State0) :-
+    store(reg(Var), #Val, _, State0).
 
-state(S0, S), [S] --> [S0].
+eval(add,A,B,C) :- C #= A + B.
+eval(mul,A,B,C) :- C #= A * B. 
+eval(mod,A,B,C) :- C #= A mod B. 
+eval(div,A,B,C) :- C #= A div B.
+eval(eql,A,B,C) :- C #<==> (A #= B).
 
 run_([], _) --> [].
-run_([(inp, Reg)|Ops], [Var|Input]) -->
-    state(State0, State1),
-    { store(Reg, #Var, State0, State1) },
-    run_(Ops, Input).
-run_([(Op, RegA, RegB)|Ops], Input) -->
-    state(State0, State1),
-    { load(RegA, State0, A)
-    , load(RegB, State0, B)
-    , op_calc(Op, A, B, Out)
-    , store(RegA, #Out, State0, State1) },
-    run_(Ops, Input).
+run_([ inp-Target | Ins ], [Var|Input]) -->
+    store(Target, #Var),
+    run_(Ins, Input).
+run_([ Op-Target-Source | Ins ], Input) -->
+    load(Target, A),
+    load(Source, B),
+    { eval(Op, A, B, Result) },
+    store(Target, #Result),
+    run_(Ins, Input).
 
-run(Input) :-
-    once(phrase_from_file(instructions(Program), "24-input.txt")),
-    length(Input, 14),
-    Input ins 1..9,
-    phrase(run_(Program, Input), [(0,0,0,0)], [(_,_,_,0)]).
+run(Program, Input, Output) :-
+    phrase(run_(Program, Input), [0,0,0,0], Output).
 
-lift(F, V, Lifted) :-
-    Lifted =.. [F, V].
+% --------------------------------------------
+% Labeling Utilities
+% --------------------------------------------
+
+wrap(Functor, Argument, Wrapped) :-
+    Wrapped =.. [Functor, Argument].
 
 maximum(Vars) :-
-    maplist(lift(max), Vars, VarsLabeled),
+    maplist(wrap(max), Vars, VarsLabeled),
     labeling(VarsLabeled, Vars).
 
 minimum(Vars) :-
-    maplist(lift(min), Vars, VarsLabeled),
+    maplist(wrap(min), Vars, VarsLabeled),
     labeling(VarsLabeled, Vars).
-
-part1(Input) :- 
-    run(Input), maximum(Input).
-
-part2(Input) :- 
-    run(Input), minimum(Input).
