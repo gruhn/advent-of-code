ad5d0f24ec3f5686d798f2334e6d2fce4df809e3
Niklas Gruhn <niklas@gruhn.me>
2022-12-01 07:25:22 +0100

    2022 / day 01 / solution

--- 2022/Day01.hs
+++ 2022/Day01.hs
@@ -1,6 +1,23 @@
 module Main where
+import Utils
+import Text.Megaparsec (sepBy, chunk)
+import Text.Megaparsec.Char (newline)
+import Text.Megaparsec.Char.Lexer (decimal)
+import Control.Applicative (some)
+import Data.List (sort)
+
+parser :: Parser [[Int]]
+parser = block `sepBy` newline
+  where
+    block = some (decimal <* newline)
 
 main :: IO ()
 main = do
+  input <- parseHardError parser <$> readFile "input/01.txt"
+  let input_sorted = reverse $ sort $ fmap sum input
+
   putStr "Part 1: "
+  print $ head input_sorted 
+
   putStr "Part 2: "
+  print $ sum $ take 3 input_sorted
