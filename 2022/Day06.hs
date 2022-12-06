fee0615ced2b1a959772e68b0f5b941f30378f66
Niklas Gruhn <niklas@gruhn.me>
2022-12-06 07:09:32 +0100

    2022 / day 06 / refactor

--- 2022/Day06.hs
+++ 2022/Day06.hs
@@ -1,21 +1,18 @@
 module Main where
+import Data.List (findIndex, tails, nub)
 
-hasDuplicate :: Eq a => [a] -> Bool
-hasDuplicate [] = False
-hasDuplicate (a:as) = 
-  a `elem` as || hasDuplicate as
+allDistinct :: Eq a => [a] -> Bool
+allDistinct as = nub as == as
 
 windows :: Int -> [a] -> [[a]]
-windows size as
-  | size > length as = []
-  | otherwise = take size as : windows size (tail as)
+windows size as = take size <$> tails as
 
 main :: IO ()
 main = do
   input <- readFile "input/06.txt"
 
   putStr "Part 1: "
-  print $ (+4) $ length $ takeWhile hasDuplicate $ windows 4 input
+  print $ (+4) <$> findIndex allDistinct (windows 4 input)
 
   putStr "Part 2: "
-  print $ (+14) $ length $ takeWhile hasDuplicate $ windows 14 input
+  print $ (+14) <$> findIndex allDistinct (windows 14 input)
