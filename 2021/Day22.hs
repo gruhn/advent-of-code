9e50356fde7911a1b9e010d1dd0d8e6111a61b94
Niklas Gruhn <niklas@gruhn.me>
2023-01-29 02:24:15 +0100

    2021 / day 22 / refactor with Cuboid library

--- 2021/Day22.hs
+++ 2021/Day22.hs
@@ -2,88 +2,50 @@ module Main where
 import Text.Parsec.String (Parser, parseFromFile)
 import Text.Parsec (string, many1, (<|>), newline, sepBy, char, option, try)
 import Text.Parsec.Char (digit)
+import Data.Foldable (foldl')
+import qualified Cuboid
+import Cuboid ( Cuboid(Cuboid), Range(Range) )
 
-type Range = (Int, Int)
+parser :: Parser [(Bool, Cuboid)]
+parser = cuboid `sepBy` newline
+    where
+        int :: Parser Int
+        int = do 
+            sign <- option "" (string "-")
+            digits <- many1 digit
+            return (read $ sign ++ digits)
 
-data Cuboid = 
-    Cuboid Range Range Range
-    deriving Show
+        range :: Parser Range
+        range = Range <$> int <* string ".." <*> int 
 
-intP :: Parser Int
-intP = do 
-    sign <- option "" (string "-")
-    digits <- many1 digit
-    return (read $ sign ++ digits)
+        cuboid_state :: Parser Bool
+        cuboid_state = try on <|> off where
+            on = True <$ string "on"
+            off = False <$ string "off"
 
-rangeP :: Parser Range
-rangeP = do
-    start <- intP
-    string ".."
-    end <- intP
-    return (start, end)
-
-cuboidStateP :: Parser Bool
-cuboidStateP = try on <|> off where
-    on = True <$ string "on"
-    off = False <$ string "off"
-
-cuboidP :: Parser (Bool, Cuboid)
-cuboidP = do
-    state  <- cuboidStateP
-    xRange <- string " x=" *> rangeP
-    yRange <- string ",y=" *> rangeP
-    zRange <- string ",z=" *> rangeP
-    return (state, Cuboid xRange yRange zRange)
-
-insideInitArea :: Cuboid -> Bool
-insideInitArea (Cuboid (x0,xn) (y0,yn) (z0,zn)) =
-    all (-50 <=) [ x0, y0, z0 ] && 
-    all (<= 50) [ xn, yn, zn ]
+        cuboid :: Parser (Bool, Cuboid)
+        cuboid = do
+            state  <- cuboid_state
+            xRange <- string " x=" *> range
+            yRange <- string ",y=" *> range
+            zRange <- string ",z=" *> range
+            return (state, Cuboid [xRange, yRange, zRange])
 
 main :: IO ()
 main = do
-    instructions <- parseFromFile (cuboidP `sepBy` newline) "input/22.txt"
-    let resultVolume = sum . map volume . foldl applyInst []
-    putStr "Part 1: "
-    print $ resultVolume . filter (insideInitArea . snd) <$> instructions
-    putStr "Part 2: "
-    print $ resultVolume <$> instructions
+    instructions <- parseFromFile parser "input/22.txt"
 
-volume :: Cuboid -> Int
-volume (Cuboid (x0,xn) (y0,yn) (z0,zn)) =
-    let xl = xn-x0+1  
-        yl = yn-y0+1 
-        zl = zn-z0+1
-        sign = signum $ minimum [ xl, yl, zl ]
-    in sign * abs (xl*yl*zl)
+    let result_volume = sum . map Cuboid.volume . foldl' applyInst []
+        init_area = Cuboid 
+            [ Range (-50) 50 
+            , Range (-50) 50
+            , Range (-50) 50 ]
+
+    putStr "Part 1: "
+    print $ result_volume . filter ((init_area `Cuboid.contains`) . snd) <$> instructions
+    putStr "Part 2: "
+    print $ result_volume <$> instructions
 
 applyInst :: [Cuboid] -> (Bool, Cuboid) -> [Cuboid]
-applyInst cs (False, c) = concatMap (`difference` c) cs
-applyInst cs (True, c) = c : concatMap (`difference` c) cs 
-
-data Axis = X Int | Y Int | Z Int
-
-slice :: Axis -> Cuboid -> (Cuboid, Cuboid)
-slice (X axis) (Cuboid (x0, xn) yr zr) =
-    ( Cuboid (x0, min (axis-1) xn) yr zr
-    , Cuboid (max x0 axis,xn) yr zr
-    )
-slice (Y axis) (Cuboid xr (y0,yn) zr) =
-    ( Cuboid xr (y0, min (axis-1) yn) zr
-    , Cuboid xr (max y0 axis,yn) zr
-    )
-slice (Z axis) (Cuboid xr yr (z0,zn)) =
-    ( Cuboid xr yr (z0, min (axis-1) zn)
-    , Cuboid xr yr (max z0 axis, zn)
-    )
-
-difference :: Cuboid -> Cuboid -> [Cuboid]
-difference c0 (Cuboid (x0,xn) (y0,yn) (z0,zn)) = 
-    let (west, c1) = slice (X x0) c0
-        (c2, east) = slice (X (xn+1)) c1
-        (south, c3) = slice (Y y0) c2
-        (c4, north) = slice (Y (yn+1)) c3
-        (below, c5) = slice (Z z0) c4
-        (c6, above) = slice (Z (zn+1)) c5
-    in filter ((> 0) . volume)
-        [ west, east, north, south, above, below ] 
+applyInst cs (False, c) = concatMap (`Cuboid.subtract` c) cs
+applyInst cs (True, c) = c : concatMap (`Cuboid.subtract` c) cs 
