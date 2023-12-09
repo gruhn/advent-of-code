ce57bbd1bcd33d64d36c5e76f9ee796120a2e6f3
Niklas Gruhn <niklas@gruhn.me>
2023-12-09 06:37:35 +0100

    2023 / day 09 / solution

--- 2023/Day09.hs
+++ 2023/Day09.hs
@@ -1,12 +1,41 @@
 module Main where
 
-import Utils (Parser, parseFile)
+import Utils (Parser, parseFile, integer)
+import Text.Megaparsec (sepEndBy, some)
+import Text.Megaparsec.Char (newline)
+import Control.Monad (guard)
+import Data.Foldable (traverse_)
+
+parser :: Parser [[Int]]
+parser = line `sepEndBy` newline
+  where
+    line :: Parser [Int]
+    line = some integer 
+
+extrapolateRight :: [Int] -> [Int]
+extrapolateRight seq
+  | all (==0) seq = seq
+  | otherwise     = seq ++ [right]
+  where
+    right = left + low
+    left = last seq
+    low = last $ extrapolateRight $ zipWith (-) (tail seq) seq
+
+extrapolateLeft :: [Int] -> [Int]
+extrapolateLeft seq
+  | all (==0) seq = seq
+  | otherwise     = left : seq
+  where
+    left = right - low
+    right = head seq
+    low = head $ extrapolateLeft $ zipWith (-) (tail seq) seq
 
 main :: IO ()
 main = do
-  -- input <- parseFile parser "input/05.txt"
+  input <- parseFile parser "input/09.txt"
 
   putStr "Part 1: "
-  -- print ""
+  print $ sum $ map (last . extrapolateRight) input
 
-  -- putStr "Part 2: "
+  putStr "Part 2: "
+  print $ sum $ map (head . extrapolateLeft) input
