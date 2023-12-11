edb1445d18c06e7988e61f0f011642869734e971
Niklas Gruhn <niklas@gruhn.me>
2023-12-11 07:33:19 +0100

    2023 / day 11 / solution

--- 2023/Day11.hs
+++ 2023/Day11.hs
@@ -1,12 +1,38 @@
 module Main where
+import Utils (withCoords, combinations)
+import qualified Data.IntSet as ISet
 
-import Utils (Parser, parseFile)
+parse :: String -> [(Int,Int)]
+parse = map fst . filter ((=='#') . snd) . withCoords . lines
+
+dist :: (Int,Int) -> (Int,Int) -> Int
+dist (x1,y1) (x2,y2) = abs (x1-x2) + abs (y1-y2)
+
+-- | adjust coordinates for expansion
+expand :: Int -> [(Int,Int)] -> [(Int,Int)]
+expand factor galaxies = map go galaxies
+  where
+    non_empty_rows = ISet.fromList $ map snd galaxies
+    non_empty_cols = ISet.fromList $ map fst galaxies
+
+    all_rows = ISet.fromList [0 .. ISet.findMax non_empty_rows] 
+    all_cols = ISet.fromList [0 .. ISet.findMax non_empty_cols] 
+
+    empty_rows = all_rows ISet.\\ non_empty_rows
+    empty_cols = all_cols ISet.\\ non_empty_cols
+
+    go :: (Int,Int) -> (Int,Int)
+    go (x,y) = (expanded_x, expanded_y)
+      where
+        expanded_x = x + (factor-1) * ISet.size (ISet.filter (<x) empty_cols) 
+        expanded_y = y + (factor-1) * ISet.size (ISet.filter (<y) empty_rows)
 
 main :: IO ()
 main = do
-  -- input <- parseFile parser "input/05.txt"
+  points <- parse <$> readFile "input/11.txt"
 
   putStr "Part 1: "
-  -- print ""
+  print $ sum $ map (uncurry dist) $ combinations $ expand 2 points
 
-  -- putStr "Part 2: "
+  putStr "Part 2: "
+  print $ sum $ map (uncurry dist) $ combinations $ expand 1000000 points
