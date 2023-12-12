c48ebb92624c60c0c39d29ed0d633dd8621afbb1
Niklas Gruhn <niklas@gruhn.me>
2023-12-12 23:52:49 +0100

    2023 / day 12 / solution

--- 2023/Day13.hs
+++ 2023/Day13.hs
@@ -2,11 +2,52 @@ module Main where
 
 import Utils (Parser, parseFile)
 
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import Text.Megaparsec.Char.Lexer
+import Control.Arrow (second, Arrow (..))
+import Control.Monad (guard)
+import Data.List (transpose)
+import Data.Foldable (traverse_)
+
+type Pattern = [String]
+
+parser :: Parser [Pattern]
+parser = pattern `sepEndBy` newline
+  where
+    pattern :: Parser Pattern
+    pattern = some (line <* newline)
+
+    line :: Parser String
+    line = some (oneOf (".#" :: String))
+
+card :: [Bool] -> Int
+card = length . filter not
+
+findMirror :: Pattern -> [Int]
+findMirror pat = do
+  index <- [1 .. length (head pat)-1]
+  guard 
+    $ (==1)
+    $ sum
+    $ map card
+    $ map (uncurry (zipWith (==)))
+    $ map (first reverse . splitAt index) pat
+
+  return index
+
 main :: IO ()
 main = do
-  -- input <- parseFile parser "input/05.txt"
+  input <- parseFile parser "input/13.txt"
 
-  putStr "Part 1: "
-  -- print ""
+  -- traverse_ (\str -> do traverse_ putStrLn str; print "") $ do 
+  print $ sum $ do
+    pat <- input
+    let vert = findMirror pat
+        hori = map (*100) $ findMirror (transpose pat)
 
+    let index = head $ vert ++ hori
+    return index
+
+  -- putStr "Part 1: "
   -- putStr "Part 2: "
