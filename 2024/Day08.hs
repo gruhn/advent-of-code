eca2c975705d0bb435fc9cfb7d8688ba0a0e16f4
Niklas Gruhn <niklas@gruhn.me>
2024-12-08 10:44:53 +0100

    2024 / day 08 / refactor

--- 2024/Day08.hs
+++ 2024/Day08.hs
@@ -7,13 +7,6 @@ import Data.List (tails)
 
 type Pos = (Int, Int)
 
-sameFrequencyPairs :: [(Pos, Char)] -> [(Pos, Pos)]
-sameFrequencyPairs frequencies = do
-  (pos1, freq1) : rest <- tails frequencies
-  (pos2, freq2) <- rest
-  guard (freq1 == freq2)
-  return (pos1, pos2)
-
 lineSegment :: Pos -> Pos -> [Pos]
 lineSegment (x1,y1) (x2,y2) = (x2,y2) : lineSegment (x2,y2) (2*x2-x1, 2*y2-y1)
   
@@ -26,19 +19,26 @@ main = do
       in_bounds :: Pos -> Bool
       in_bounds (x,y) = 0 <= x && x <= max_x && 0 <= y && y <= max_y
 
-      frequencies :: [(Pos, Char)]
-      frequencies = filter ((/= '.') . snd) input
+      antennas :: [(Pos, Char)]
+      antennas = filter ((/= '.') . snd) input
+
+      same_frequency_pairs :: [(Pos, Pos)]
+      same_frequency_pairs = do
+        (pos1, freq1) : rest <- tails antennas
+        (pos2, freq2) <- rest
+        guard $ freq1 == freq2
+        return (pos1, pos2)
 
   putStr "Part 1: "
   print $ length $ nubOrd $ do
-    (pos1, pos2) <- sameFrequencyPairs frequencies
-    let antinode1 = lineSegment pos1 pos2 !! 1
-    let antinode2 = lineSegment pos2 pos1 !! 1
-    filter in_bounds [antinode1, antinode2]
+    (pos1, pos2) <- same_frequency_pairs
+    let anti_node1 = lineSegment pos1 pos2 !! 1
+    let anti_node2 = lineSegment pos2 pos1 !! 1
+    filter in_bounds [anti_node1, anti_node2]
 
   putStr "Part 2: "
   print $ length $ nubOrd $ do
-    (pos1, pos2) <- sameFrequencyPairs frequencies
-    let anitnodes1 = takeWhile in_bounds $ lineSegment pos1 pos2
-    let anitnodes2 = takeWhile in_bounds $ lineSegment pos2 pos1
-    anitnodes1 ++ anitnodes2 
+    (pos1, pos2) <- same_frequency_pairs
+    let anti_nodes1 = takeWhile in_bounds $ lineSegment pos1 pos2
+    let anti_nodes2 = takeWhile in_bounds $ lineSegment pos2 pos1
+    anti_nodes1 ++ anti_nodes2
