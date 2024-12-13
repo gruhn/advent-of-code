806a666a67efb3c967b864ad277aa4f4de437135
Niklas Gruhn <niklas@gruhn.me>
2024-12-13 18:32:35 +0100

    2024 / day 13 / simplify

--- 2024/Day13.hs
+++ 2024/Day13.hs
@@ -1,104 +1,74 @@
 module Main (main) where
-import Utils (Parser, integer, parseFile, safeMinimum)
+import Utils (Parser, integer, parseFile)
 import Text.Megaparsec.Char (string, newline)
-import Text.Megaparsec (sepBy, sepEndBy)
+import Text.Megaparsec (sepEndBy)
+import Data.Maybe (maybeToList)
 import Control.Monad (guard)
-import Data.Foldable (traverse_)
-import Debug.Trace (traceShowM)
-import Data.Ratio (Ratio, (%))
-import Data.Maybe (maybeToList, catMaybes)
-import Theory.LinearArithmatic.BranchAndBound (branchAndBound)
-import Theory.LinearArithmatic.Constraint (Constraint, Var, AffineExpr (AffineExpr), (.==), (.>=), Assignment, (.<=))
-import qualified Data.IntMap as IntMap
-import qualified Data.IntSet as IntSet
 
-data Vec = Vec 
-  { x :: Rational
-  , y :: Rational
-  } deriving Show
+data Vec = Vec { x :: Rational, y :: Rational } 
+  deriving Show
 
 type ClawMachineConfig = (Vec, Vec, Vec)
 
 parser :: Parser [ClawMachineConfig]
-parser = claw_machine_conifg `sepEndBy` newline
+parser = claw_machine_config `sepEndBy` newline
   where
-    claw_machine_conifg :: Parser ClawMachineConfig
-    claw_machine_conifg = do
-      buttonA <- string "Button A: " *> button    <* newline
-      buttonB <- string "Button B: " *> button    <* newline
-      prize   <- string "Prize: "    *> prize_pos <* newline
-      return (buttonA, buttonB, prize)
+    claw_machine_config :: Parser ClawMachineConfig
+    claw_machine_config = (,,)
+      <$ string "Button A: " <*> button <* newline
+      <* string "Button B: " <*> button <* newline
+      <* string "Prize: "    <*> prize  <* newline
+
+    rational :: Parser Rational
+    rational = fromIntegral <$> integer
 
     button :: Parser Vec
-    button = do 
-      string "X+" 
-      dx <- fromIntegral <$> integer 
-      string ", Y+" 
-      dy <- fromIntegral <$> integer
-      return $ Vec dx dy
+    button = Vec 
+      <$ string "X+" <*> rational <* string ", "
+      <* string "Y+" <*> rational
 
-    prize_pos :: Parser Vec
-    prize_pos = do 
-      string "X=" 
-      px <- fromIntegral <$> integer 
-      string ", Y=" 
-      py <- fromIntegral <$> integer
-      return $ Vec px py
+    prize :: Parser Vec
+    prize = Vec 
+      <$ string "X=" <*> rational <* string ", "
+      <* string "Y=" <*> rational
 
-costA, costB :: Rational
-costA = 3
-costB = 1
+isIntegral :: Rational -> Bool
+isIntegral num = fromIntegral (floor num) == num
 
-varA, varB :: Int
-varA = 1
-varB = 2
+solve :: ClawMachineConfig -> Maybe (Integer, Integer)
+solve (buttonA, buttonB, prize) = do
+  -- Gauss elimination:
+  let factorA_denom = buttonB.y * buttonA.x - buttonB.x * buttonA.y
+  guard $ factorA_denom /= 0
+  let factorA = (prize.x * buttonB.y - prize.y * buttonB.x) / factorA_denom
+  guard $ buttonB.x /= 0 
+  let factorB = (prize.x - buttonA.x * factorA) / buttonB.x
+  -- Check additional constraints:
+  guard $ factorA >= 0 && isIntegral factorA
+  guard $ factorB >= 0 && isIntegral factorB
+  return (floor factorA, floor factorB)
 
-eval :: Assignment -> Rational
-eval assign = sum $ IntMap.unionWith (*) assign (IntMap.fromList [(varA, costA), (varB, costB)])
-
-constant :: Rational -> AffineExpr
-constant c = AffineExpr c IntMap.empty
-
-toConstraints :: ClawMachineConfig -> [Constraint]
-toConstraints (buttonA, buttonB, prize) = 
-  let 
-    factorA = AffineExpr 0 (IntMap.singleton 1 1)
-    factorB = AffineExpr 0 (IntMap.singleton 2 1)
-  in
-    [ factorA * constant buttonA.x + factorB * constant buttonB.x .== constant prize.x
-    , factorA * constant buttonA.y + factorB * constant buttonB.y .== constant prize.y
-    , factorA .>= 0
-    , factorB .>= 0
-    ]   
-
-solveOpt :: [Constraint] -> (Rational, Rational) -> Maybe Rational
-solveOpt constraints (lo, hi) = 
-  let
-    factorA = AffineExpr 0 (IntMap.singleton varA 1)
-    factorB = AffineExpr 0 (IntMap.singleton varB 1)
-
-    lower_bound = constant lo .<= factorA
-    upper_bound = factorA .<= constant hi
-  in do
-    sol <- branchAndBound (lower_bound : upper_bound : constraints) (IntSet.fromList [1,2])
-    let value = eval sol
-    if lo == hi then
-      return value   
-    else 
-      return $ minimum $ (value:) $ catMaybes [ solveOpt constraints (lo, hi/2), solveOpt constraints (hi/2, lo) ]
+adjustPrize :: (Rational -> Rational) -> ClawMachineConfig -> ClawMachineConfig
+adjustPrize adjust (buttonA, buttonB, prize) = 
+  (buttonA, buttonB, Vec (adjust prize.x) (adjust prize.y))
 
 main :: IO ()
 main = do
   input <- parseFile parser "input/13.txt"
 
+  let cost :: Integer -> Integer -> Integer
+      cost factorA factorB = 3 * factorA + factorB
+
   putStr "Part 1: "
   print $ sum $ do
     config <- input
-    maybeToList $ solveOpt (toConstraints config) (0, 100)
+    (factorA, factorB) <- maybeToList $ solve config
+    guard $ factorA <= 100
+    guard $ factorB <= 100
+    return $ cost factorA factorB
 
   putStr "Part 2: "
   print $ sum $ do
-    (buttonA, buttonB, Vec prizeX prizeY) <- input
-    let config' = (buttonA, buttonB, Vec (prizeX+10000000000000) (prizeY+10000000000000))
-    maybeToList $ solveOpt (toConstraints config') (0, 10^20)
-    
+    config <- adjustPrize (+10000000000000) <$> input
+    (factorA, factorB) <- maybeToList $ solve config
+    return $ cost factorA factorB
