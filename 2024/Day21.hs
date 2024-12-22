ae11478daff709250a0c7ae0b2ebfd1a381b52b3
Niklas Gruhn <niklas@gruhn.me>
2024-12-22 22:43:47 +0100

    2024 / day 21 / part 1

--- 2024/Day21.hs
+++ 2024/Day21.hs
@@ -4,18 +4,33 @@ import Data.Foldable (traverse_)
 import Data.Map (Map)
 import qualified Data.Map as Map
 import Control.Monad (guard)
+import Data.List (group, maximumBy, isPrefixOf)
+import Data.Ord (comparing)
+import Data.Maybe (catMaybes, maybeToList)
+import Algorithm.Search (dijkstra)
+import Utils (minimaBy)
 
-type Keypad = Map (Char, Char) [String]
+-- data Node = Node Char Char Int
+--   deriving (Eq, Ord, Show)
+
+-- data Symbol = NonTerm Node | Term Char
+
+-- type RuleSet = Map Node [[Symbol]]
+
+type Keypad = Map Char (Map Char Char)
+
+mkEdge :: Char -> Char -> Char -> (Char, Map Char Char)
+mkEdge source dir target = (source, Map.singleton dir target)
 
 keypadRow :: [Char] -> Keypad
-keypadRow row = Map.fromList $ do
+keypadRow row = Map.fromListWith Map.union $ do
   (left, right) <- zip row (tail row)
-  [((left, right), pure ">"), ((right, left), pure "<")]
+  [ mkEdge left '>' right, mkEdge right '<' left ]
 
 keypadCol :: [Char] -> Keypad
-keypadCol col = Map.fromList $ do
+keypadCol col = Map.fromListWith Map.union $ do
   (top, bottom) <- zip col (tail col)
-  [((top, bottom), pure "v"), ((bottom, top), pure "^")]
+  [ mkEdge top 'v' bottom, mkEdge bottom '^' top ]
 
 {- 
   +---+---+---+
@@ -29,12 +44,12 @@ keypadCol col = Map.fromList $ do
       +---+---+ 
 -}
 numericKeypad :: Keypad
-numericKeypad = 
-  transitiveClosure $ Map.unions
+numericKeypad =
+  Map.unionsWith Map.union
     [ keypadRow "789"
     , keypadRow "456"
     , keypadRow "123"
-    , keypadRow "0A"  
+    , keypadRow "0A"
     , keypadCol "741"
     , keypadCol "8520"
     , keypadCol "963A"
@@ -48,8 +63,8 @@ numericKeypad =
   +---+---+---+ 
 -}
 directionalKeypad :: Keypad
-directionalKeypad = 
-  transitiveClosure $ Map.unions
+directionalKeypad =
+  Map.unionsWith Map.union
     [ keypadRow "^A"
     , keypadRow "<v>"
     , keypadCol "<"
@@ -57,58 +72,154 @@ directionalKeypad =
     , keypadCol "A>"
     ]
 
-shortest :: [String] -> [String] -> [String]
-shortest paths1 paths2
-  | length (head paths1) < length (head paths2) = paths1
-  | length (head paths1) > length (head paths2) = paths2
-  | otherwise = paths1 ++ paths2
+type State = ([Char], String)
 
-transitiveClosure :: Keypad -> Keypad
-transitiveClosure keypad = 
-  let
-    transitive_paths :: Keypad
-    transitive_paths = Map.fromListWith shortest $ do
-      ((src , mid), src_to_mids) <- Map.toList keypad
-      ((mid', trg), mid_to_trgs) <- Map.toList keypad
-      if src == trg then
-        return ((src, src), pure "")
-      else do
-        guard $ mid' == mid
-        src_to_mid <- src_to_mids
-        mid_to_trg <- mid_to_trgs
-        return ((src, trg), [src_to_mid ++ mid_to_trg])
+move :: Char -> State -> Maybe State
+move dir ([], _) = error "move: []"
+move dir ([pos], output) = 
+  case Map.lookup pos numericKeypad of
+    Nothing      -> error "move -> numericKeypad -> Nothing"
+    Just targets -> do 
+      new_pos <- Map.lookup dir targets
+      return ([new_pos], output)
+move dir (pos : poses, output) = 
+  case Map.lookup pos directionalKeypad of
+    Nothing      -> error "move -> numericKeypad -> Nothing"
+    Just targets -> do 
+      new_pos <- Map.lookup dir targets
+      return (new_pos : poses, output)
 
-    new_keypad :: Keypad
-    new_keypad = Map.unionWith shortest keypad transitive_paths
-  in
-    if keypad == new_keypad then
-      keypad
-    else
-      transitiveClosure new_keypad
+press :: State -> Maybe State
+press ([], _) = error "press: []"
+press ([pos], output) = Just ([pos], pos:output)
+press (pos : poses, output) = do
+  (poses', output') <- 
+    if pos == 'A' then 
+      press (poses, output)
+    else 
+      move pos (poses, output)
+  return (pos : poses', output')
 
-numericKeypadPaths :: String -> [String]
-numericKeypadPaths []  = return []
-numericKeypadPaths [_] = return []
-numericKeypadPaths (button1 : button2 : buttons) = do
-  step_path <- numericKeypad Map.! (button1, button2) 
-  rest_path <- numericKeypadPaths (button2 : buttons)
-  return $ step_path ++ "A" ++ rest_path
+next :: String -> State -> [State]
+next target state = presses ++ moves
+  where
+    moves :: [State]
+    moves = catMaybes [ move dir state | dir <- "^v<>" ]
+
+    presses :: [State]
+    presses = do
+      (poses, output) <- maybeToList $ press state
+      guard $ reverse output `isPrefixOf` target
+      return (poses, output)
+
+-- neighbors :: Char -> Keypad -> [(Char, Char)]
+-- neighbors source keypad =
+--   case Map.lookup source keypad of
+--     Nothing      -> error $ "missing: " ++ [source]
+--     Just targets -> Map.toList targets
+
+-- expand :: Node -> [[Symbol]]
+-- expand (Node source target level) =
+--   if source == target then
+--     return []
+--   else if level == 0 then do
+--     (dir, neighbor) <- neighbors source numericKeypad
+--     return 
+--       [ NonTerm $ Node 'A' dir (level+1)
+--       , Term 'A'
+--       , NonTerm $ Node dir 'A' (level+1)
+--       , Term 'A' 
+--       , NonTerm $ Node neighbor target level
+--       ]
+--   else do
+--     (dir, neighbor) <- neighbors source directionalKeypad
+--     return 
+--       [ NonTerm $ Node 'A' dir (level+1)
+--       , Term 'A'
+--       , NonTerm $ Node dir 'A' (level+1)
+--       , Term 'A' 
+--       , NonTerm $ Node neighbor target level 
+--       ]
+  
+-- shortest :: String -> String -> String
+-- shortest path1 path2
+--   | length path1 < length path2 = path1
+--   | length path1 > length path2 = path2
+--   | otherwise = maximumBy (comparing score) [path1, path2]
+--   where
+--     score :: String -> Int
+--     score = sum . map ((^2) . length) . group
+
+-- transitiveClosure :: Keypad -> Keypad
+-- transitiveClosure keypad =
+--   let
+--     transitive_paths :: Keypad
+--     transitive_paths = Map.fromListWith shortest $ do
+--       ((src , mid), src_to_mid) <- Map.toList keypad
+--       ((mid', trg), mid_to_trg) <- Map.toList keypad
+--       if src == trg then
+--         return ((src, src), "")
+--       else do
+--         guard $ mid' == mid
+--         return ((src, trg), src_to_mid ++ mid_to_trg)
+
+--     new_keypad :: Keypad
+--     new_keypad = Map.unionWith shortest keypad transitive_paths
+--   in
+--     if keypad == new_keypad then
+--       keypad
+--     else
+--       transitiveClosure new_keypad
+
+-- numericKeypadPaths :: String -> [String]
+-- numericKeypadPaths []  = return []
+-- numericKeypadPaths [_] = return []
+-- numericKeypadPaths (button1 : button2 : buttons) = do
+--   step_path <- numericKeypad Map.! (button1, button2) 
+--   rest_path <- numericKeypadPaths (button2 : buttons)
+--   return $ step_path ++ "A" ++ rest_path
+
+-- numericKeypadPath :: String -> String
+-- numericKeypadPath buttons = do
+--   step <- zip buttons (tail buttons)
+--   numericKeypad Map.! step ++ "A"
 
 -- directionalKeypadPath :: String -> String
 -- directionalKeypadPath buttons = do
 --   step <- zip buttons (tail buttons)
 --   directionalKeypad Map.! step ++ "A"
 
+-- newtype DirMap = DirMap (Map (Char, Char) Path)
+
+-- data PathSegment = Unit String | Or String String
+
+-- type Path = [PathSegment]
+
+-- instance Semigroup DirMap where
+--   DirMap edges1 <> DirMap edges2 = DirMap $ Map.map embed_each edges1
+--     where
+--       embed_each :: Path -> Path
+--       embed_each = minimaBy (comparing length) . concatMap embed
+
+--       embed :: Path -> Path
+--       embed path = do
+--         (source, target) <- zip ('A' : path) path
+--         edges2 Map.! (source, target)
+      
 main :: IO ()
 main = do
   codes <- lines <$> readFile "input/21.txt"
 
-  traverse_ print $ do
+  print $ sum $ catMaybes $ do 
     code <- codes
-    let leading_nums :: Int
+    let start_state = ("AAA", "")
+        cost _ _ = 1
+        is_target (_, output) = reverse output == code 
+
+        leading_nums :: Int
         leading_nums = read $ init code
 
-        shortest_seq :: [String]
-        shortest_seq = numericKeypadPaths $ 'A' : code
+    return $ do 
+      (path_len, _) <- dijkstra (next code) cost is_target start_state
+      return (path_len * leading_nums)
 
-    return (leading_nums, shortest_seq)
