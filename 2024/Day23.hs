33cfcb627ba6bca4043ad0ee08d93240a3eeb038
Niklas Gruhn <niklas@gruhn.me>
2024-12-23 20:05:26 +0100

    2024 / day 23 / clean up

--- 2024/Day23.hs
+++ 2024/Day23.hs
@@ -1,86 +1,89 @@
 module Main (main) where
-import Utils (Parser, parseFile, select)
+import Utils (Parser, parseFile)
 import Text.Megaparsec (some, sepEndBy)
 import Text.Megaparsec.Char (lowerChar, string, newline)
-import Data.Containers.ListUtils (nubOrd)
-import Data.Tuple (swap)
 import Control.Monad (guard)
 import Data.Map.Strict (Map)
-import Data.List (tails, sort, isPrefixOf, sortOn, intercalate)
+import Data.List (isPrefixOf, intercalate)
 import qualified Data.Map as Map
-import Data.Foldable (asum, traverse_, for_)
-import Data.Maybe (fromMaybe, listToMaybe)
-import Data.List (find)
 import qualified Data.Set as Set
 import Data.Set (Set)
-import Data.Foldable (maximumBy)
-import Data.Ord (comparing, Down (Down))
-import Debug.Trace
 
-parser :: Parser [(String, String)]
-parser = line `sepEndBy` newline
+type Graph = Map String (Set String)
+
+fromEdges :: [(String, String)] -> Graph
+fromEdges edges = Map.fromListWith Set.union $ do
+  (source, target) <- edges
+  [ (source, Set.singleton target), (target, Set.singleton source) ]
+
+parser :: Parser Graph
+parser = fromEdges <$> edge `sepEndBy` newline
   where
-    line :: Parser (String, String)
-    line = do
+    edge :: Parser (String, String)
+    edge = do
       from <- some lowerChar
       string "-"
       to  <- some lowerChar
       return (from, to)
 
--- step :: [(String, String)] -> [[String]] -> [[String]]
--- step edges groups = fromMaybe groups $ asum $ do
---   (g, gs) : rest_groups <- tails $ Map.toList groups
---   case find (\(g2, gs) -> (g, g2) `elem` edges) rest_groups of
---     Nothing -> return Nothing
---     Just (g2, gs2) ->
---       return $ Just $ Map.insertWith (++) g gs2 $ Map.delete g2 groups
+isClique :: Graph -> Set String -> Bool
+isClique graph nodes = and $ do
+  node <- Set.toList nodes
+  return $ Set.isSubsetOf nodes $ Set.insert node (graph Map.! node)
 
-insert :: [(String, String)] -> String -> [[String]] -> [[String]]
-insert edges item [] = [[item]]
-insert edges item (group : groups) =
-  if all (\it -> (item, it) `elem` edges) group then
-    (item:group) : insert edges item groups
-  else
-    group : insert edges item groups
+sublists :: Int -> [a] -> [[a]]
+sublists size list 
+  | size <  0 = undefined
+  | size == 0 = [[]]
+  | otherwise = 
+    case list of 
+      []     -> []
+      (a:as) -> [ a:as' | as' <- sublists (size-1) as ] ++ sublists size as
 
-subsets :: [a] -> [[a]]
-subsets []     = [[]]
-subsets (a:as) = map (a:) (subsets as) ++ subsets as
+subsets :: Ord a => Int -> Set a -> [Set a]
+subsets size = map Set.fromList . sublists size . Set.toList
 
-buckets :: Set (String, String) -> [String] -> [[String]]
-buckets edges [] = []
-buckets edges nodes = nubOrd $ do
-  node1 : rest1 <- tails nodes
-  bucket <- [] : buckets edges rest1
-  guard $ and $ do
-    node2 <- bucket
-    return $ (node1, node2) `Set.member` edges
-  return $ sort (node1:bucket)
+deleteNode :: String -> Graph -> Graph
+deleteNode node graph = 
+  case Map.lookup node graph of
+    Nothing        -> graph
+    Just neighbors -> 
+      Map.delete node $ foldr (Map.adjust (Set.delete node)) graph neighbors
 
-allConnected :: Map String (Set String) -> Set String -> Bool
-allConnected edges str_set = and $ do
-  node <- Set.toList str_set
-  return $ Set.isSubsetOf str_set $ Set.insert node (edges Map.! node)
+maxOutDegree :: Graph -> Int
+maxOutDegree = maximum . Map.map length
 
-go :: Map String (Set String) -> [Set String]
-go edges = do
-  (node, neighbors) <- Map.toList edges
-  (_, rest) <- select $ Set.toList neighbors
-  let rest_set = Set.fromList rest
-  guard $ allConnected edges rest_set
-  return $ Set.insert node rest_set
+cliques :: Graph -> Int -> [Set String]
+cliques graph clique_size =  
+  case Map.lookupMax graph of
+    Nothing                -> []
+    Just (node, neighbors) -> 
+      let
+        cliques_with_node :: [Set String]
+        cliques_with_node = do 
+          subset <- subsets (clique_size-1) neighbors
+          guard $ isClique graph subset
+          return $ Set.insert node subset
+
+        cliques_without_node :: [Set String]
+        cliques_without_node =
+          cliques (deleteNode node graph) clique_size
+      in
+        cliques_with_node ++ cliques_without_node
+
+formatAnswer :: Set String -> String
+formatAnswer = intercalate "," . Set.toAscList
 
 main :: IO ()
 main = do
-  input <- parseFile parser "input/23.txt"
+  graph <- parseFile parser "input/23.txt"
 
-  let comps = nubOrd $ concat $ [ [from, to] | (from, to) <- input ]
-      input' = Set.fromList $ map swap input ++ input
+  putStr "Part 1: "
+  print $ length $ do 
+    clique <- cliques graph 3
+    guard $ any ("t" `isPrefixOf`) clique
 
-      edges :: Map String (Set String)
-      edges = Map.fromListWith (<>) $ do
-        (from, to) <- input
-        [ (from, Set.singleton to), (to, Set.singleton from) ]
-
-  for_ (map Set.toAscList $ go edges) $ \group -> do -- Map.unionWith (<>) edges $ Map.fromList [ (node, Set.singleton node) | node <- comps ]
-    putStrLn $ intercalate "," group
+  putStr "Part 2: "
+  putStrLn $ formatAnswer $ head $ do 
+    clique_size <- reverse [3 .. maxOutDegree graph]
+    cliques graph clique_size
